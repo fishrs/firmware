7e35e55651fa40d2607404876732ab877832eb1d
Braden Everson <bradeneverson@gmail.com>
2024-07-01 07:31:55 -0500

    Splash

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut lcd = Ili9341Lcd::new(24, 25, 18, (Bus::Spi0, SlaveSelect::Ss0, 32_000_000, Mode::Mode0), LCD_WIDTH, LCD_HEIGHT).expect("Init lcd");
     lcd.init();
 
-    let img = image::open("test.jpg").expect("Failed to open image");
+    let img = image::open("splash.jpg").expect("Failed to open image");
 
     // Resize the image to fit the LCD screen
     let img = img.resize(LCD_WIDTH as u32, LCD_HEIGHT as u32, image::imageops::FilterType::Nearest);
