064a0f925e1b5f582e86423b5a484e42531ef3ee
Braden Everson <bradeneverson@gmail.com>
2024-07-02 10:06:57 -0500

    Configure depth reading

--- src/fish.rs
+++ src/fish.rs
@@ -1,5 +1,6 @@
-use std::error::Error;
+use std::{error::Error, fs::File, io::BufReader};
 
+use mappy::lake_reader::depth_lines::DepthLines;
 use rppal::uart::Parity;
 use unda::core::{data::input::Input, network::Network};
 
@@ -9,22 +10,27 @@ pub struct FishRs {
     reader: DataReader,
     output: MotorDriver,
     model: Network,
-    response_thresh: f32
+    response_thresh: f32,
+    depth_reader: DepthLines
 }
 
 impl FishRs {
-    pub fn new(reader_args: ((u32, Parity, u8, u8), u8), model_file: &str, res_thresh: f32) -> Result<Self, Box<dyn Error>> {
+    pub fn new(reader_args: ((u32, Parity, u8, u8), u8), model_file: &str, depth_file: &str, res_thresh: f32) -> Result<Self, Box<dyn Error>> {
+        let fish_file = File::open(depth_file)?;
+
+        let reader = BufReader::new(fish_file);
         Ok(Self {
             reader: DataReader::new(reader_args.0, reader_args.1)?,
             output: MotorDriver {},
             model: Network::load(model_file),
+            depth_reader: serde_json::from_reader(reader)?,
             response_thresh: res_thresh
         })
     }
 
     pub fn beat(&mut self) {
         let ((lon, lat), strain) = self.reader.read();
-        let depth = 0f64;
+        let depth = self.depth_reader.get_depth_at((lon, lat));
 
         let input = vec![depth as f32, strain as f32];
         let model_response = self.model.predict(&input);
