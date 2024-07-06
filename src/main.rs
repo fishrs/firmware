7f6ce6c6bd3ac78060f0b17ed89a6141ea9e99af
Braden Everson <bradeneverson@gmail.com>
2024-07-06 17:17:04 -0500

    Implement subway surfers mode (brainrot)

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,25 @@ use rppal::{spi::{Bus, Mode, SlaveSelect}, uart::Parity};
 const LCD_WIDTH: usize = 240;
 const LCD_HEIGHT: usize = 320;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut lcd = Ili9341Lcd::new(24, 25, 18, (Bus::Spi0, SlaveSelect::Ss0, 32_000_000, Mode::Mode0), LCD_WIDTH, LCD_HEIGHT).expect("Init lcd");
-    lcd.init();
-
-    let img = image::open("splash.jpg").expect("Failed to open image");
+pub fn draw_img(lcd: &mut Ili9341Lcd, img_str: &str) {
+    let img = image::open(img_str).expect("Failed to open image");
 
     // Resize the image to fit the LCD screen
     let img = img.resize(LCD_WIDTH as u32, LCD_HEIGHT as u32, image::imageops::FilterType::Nearest);
 
     // Send the image to the LCD
     lcd.send_image(&img.to_rgb8()).expect("Write image");
+}
 
-    let reader = DataReader::new((9600, Parity::None, 8, 1), 5/*Change*/)?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut lcd = Ili9341Lcd::new(24, 25, 18, (Bus::Spi0, SlaveSelect::Ss0, 32_000_000, Mode::Mode0), LCD_WIDTH, LCD_HEIGHT).expect("Init lcd");
+    lcd.init();
+
+    draw_img(&mut lcd, "frames/subway0001.jpg");
+
+    loop {}
+
+    /*let reader = DataReader::new((9600, Parity::None, 8, 1), 5/*Change*/)?;
 
     //let mut data_log = vec![];
 
@@ -41,5 +47,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         
 
         std::thread::sleep(Duration::from_millis(500))
-    }
+    }*/
 }
